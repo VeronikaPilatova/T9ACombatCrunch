67cf7bffd4f4890b75cdd6a62bd06e1163dfeda2
VeronikaPilatova <pilatova.v@centrum.cz>
2019-02-12 22:17:09 +0100

    Created layout for new stats activity

---
+++ app/src/main/java/com/the_ninth_age/t9acombatcrunch/Activity/StatsActivity.java
@@ -0,0 +1,15 @@
+package com.the_ninth_age.t9acombatcrunch.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.the_ninth_age.t9acombatcrunch.R;
+
+public class StatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+    }
+}
